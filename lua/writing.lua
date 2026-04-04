3558d8ffea88d904b90d22be2e598dbda0cadd8d
Rémy <remy.vandyk@ovrsea.com>
2026-04-04 22:23:56 +0200

    better markdown

--- lua/writing.lua
+++ lua/writing.lua
@@ -1,1 +1,14 @@
 vim.pack.add({"https://github.com/folke/zen-mode.nvim"})
+require("zen-mode").setup({
+    window = {
+    width = 80,             -- largeur idéale pour la prose
+    options = {
+      spell = true,
+      spelllang = "fr,en",
+      wrap = true,
+      linebreak = true,
+    },
+  }
+})
+
+vim.pack.add({'https://github.com/MeanderingProgrammer/render-markdown.nvim'})
